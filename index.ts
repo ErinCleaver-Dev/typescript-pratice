73082937094cc3a509d266893cd6a6a30a0acca5
ErinCleaver-GRCC <77419665+ErinCleaver-Dev@users.noreply.github.com>
2023-12-11 09:26:41 -0500

    Notes on types

---
+++ index.ts
@@ -0,0 +1,18 @@
+//has the type of date.  
+// has a internal def of what data is
+const today = new Date();
+
+console.log(today.getMonth())
+
+// is a object that is a person
+const person: object = {
+    age: 20
+}
+
+
+// a class
+class Color {
+
+}
+
+const red = new Color();
