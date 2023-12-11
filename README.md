becdbd5ae4804c10c477ec359900fb2cd066436b
ErinCleaver-GRCC <77419665+ErinCleaver-Dev@users.noreply.github.com>
2023-12-11 12:16:26 -0500

    Notes on functions and objects

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 S3 - Functions and Objects:
 *  Annotations for functions:  Code we add to tell typescript what type of arguments a function will receive and what type of values it will return.
 *  Inference for functions:  Type script types to figure out what type of value a function will return.  
+*  No type inference for arguments
+    *  With fucntions you always have to tell it the type for the intup variables in typescript. 
+*  You should always include a annotation for the return value, to make sure you returned a value.  
+
--- index.ts
+++ index.ts
@@ -1,8 +1,74 @@
 // a function to add things
 // tell it that it is to add a number
 // must have a return, it cannot be void.
+
+// will automically check to make sure your returning something inside of the function
+// dose not keep track of human logic. 
 const add = (num1: number, num2: number): number => {
     return num1 + num2
 };
 
 console.log(add(5, 4))
+
+// will make sure you return a number, also gives a error if you do not have return
+const subtract = (num1: number, num2: number): number => {
+    return num1 - num2
+}
+
+console.log(subtract(5, 4))
+
+function divide (num1: number, num2: number): number {
+    return num1/num2
+}
+
+console.log(divide(5, 4))
+
+const multiply = function(num1:number, num2:number): number {
+    return num1 * num2
+}
+
+console.log(multiply(5, 4))
+
+//to tell a function that it will not return somthing you use void
+const logger = (message: string):void => {
+    console.log(message);
+}
+
+const errorMessage = (message: string): never => {
+    throw new Error(message);
+}
+
+const todaysForcast = {
+    date: new Date(),
+    weather: 'sunny'
+}
+
+// will always be to different statements 
+const printWeather = ({date, weather}: {date: Date, weather: string}): void => { 
+    console.log(date);
+    console.log(weather)
+}
+
+printWeather(todaysForcast)
+    
+
+// objects
+const profile = {
+    name: 'April',
+    age: 20, 
+    coords: {
+        lat: 0, lng: 15
+    },
+    setAge(age: number) {
+        this.age = age;
+    }
+};
+
+// have to write out the expected stucture of age when working with objects
+// only works with js 2015 and later
+const { age }: {age: number} = profile;
+
+
+const { coords: {lat, lng}}: 
+{coords: {lat: number; lng:number}} = profile
+
